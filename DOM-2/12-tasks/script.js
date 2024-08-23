7b3112736b71e7b9644bfa83ab01833ea6fe271f
msruan <santosruan2021@gmail.com>
2024-08-23 15:56:08 -0300

    DOM-2 feat: continuar a questao 12

--- DOM-2/12-tasks/script.js
+++ DOM-2/12-tasks/script.js
@@ -1,42 +1,70 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var utils_1 = require("../lib/utils");
 document.addEventListener("DOMContentLoaded", main);
 var tasks = [];
+var contadorId = 1;
 function main() {
-    var button = (0, utils_1.$)("adicionarBtn");
+    console.log("entrei na main");
+    var button = $("adicionarBtn");
     button === null || button === void 0 ? void 0 : button.addEventListener("click", addTask);
 }
-function updateRows() {
+function concludeTask(button) {
     var _a, _b;
-    var tbody = (_a = (0, utils_1.$)("tabelaTarefas")) === null || _a === void 0 ? void 0 : _a.querySelector("tbody");
+    console.log("lindinho");
+    var trow = (_a = button.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement;
+    var id = Array.from((_b = trow === null || trow === void 0 ? void 0 : trow.getElementsByClassName("taskId")) !== null && _b !== void 0 ? _b : []).at(0);
+    console.log(id === null || id === void 0 ? void 0 : id.textContent);
+    var task = tasks.find(function (task) { return task.id === Number(id === null || id === void 0 ? void 0 : id.textContent); });
+    console.log(task);
+    if (task) {
+        task.dataConclusao = new Date();
+        updateRows();
+    }
+}
+function updateRows() {
+    var _a, _b, _c, _d;
+    var tbody = (_a = $("tabelaTarefas")) === null || _a === void 0 ? void 0 : _a.querySelector("tbody");
+    var existingTrows = Array.from((_b = document.getElementsByClassName("taskId")) !== null && _b !== void 0 ? _b : []).map(function (id) { var _a; return (_a = Number(id.textContent)) !== null && _a !== void 0 ? _a : NaN; });
+    var filteredTasks = tasks.filter(function (task) { return !existingTrows.find(function (id) { return id === task.id; }); });
     var trows = [];
-    for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
-        var task = tasks_1[_i];
+    var _loop_1 = function (task) {
         var trow = document.createElement("tr");
         var id = document.createElement("td");
         var descricao = document.createElement("td");
         var dataInicio = document.createElement("td");
         var dataConclusao = document.createElement("td");
+        var actionsButtons = document.createElement("td");
+        var finishButton = document.createElement("button");
+        var excludeButton = document.createElement("button");
+        id.className = "taskId";
         id.textContent = "".concat(task.id);
         descricao.textContent = "".concat(task.descricao);
         dataInicio.textContent = "".concat(task.dataInicio.toLocaleDateString());
-        dataConclusao.textContent = "".concat((_b = task.dataConclusao) === null || _b === void 0 ? void 0 : _b.toLocaleDateString());
+        dataConclusao.textContent = "".concat((_d = (_c = task.dataConclusao) === null || _c === void 0 ? void 0 : _c.toLocaleDateString()) !== null && _d !== void 0 ? _d : "----");
+        finishButton.textContent = "Concluir";
+        finishButton.addEventListener("click", function () { return concludeTask(finishButton); });
+        excludeButton.textContent = "Excluir";
+        actionsButtons.appendChild(finishButton);
+        actionsButtons.appendChild(excludeButton);
         trow.appendChild(id);
         trow.appendChild(descricao);
         trow.appendChild(dataInicio);
         trow.appendChild(dataConclusao);
+        trow.appendChild(actionsButtons);
         trows.push(trow);
+    };
+    for (var _i = 0, filteredTasks_1 = filteredTasks; _i < filteredTasks_1.length; _i++) {
+        var task = filteredTasks_1[_i];
+        _loop_1(task);
     }
     if (trows.length) {
         trows.forEach(function (tr) { return tbody === null || tbody === void 0 ? void 0 : tbody.appendChild(tr); });
     }
 }
 function addTask() {
-    var input = (0, utils_1.$)("descricaoTarefa");
+    console.log("chamei da");
+    var input = $("descricaoTarefa");
     if (input === null || input === void 0 ? void 0 : input.value) {
         var newTask = {
-            id: tasks.length + 1,
+            id: contadorId++,
             descricao: input.value.trim(),
             dataInicio: new Date(),
         };
@@ -44,6 +72,23 @@ function addTask() {
         updateRows();
     }
     else {
-        (0, utils_1.addError)("descricaoTarefa", "Passa alguma coisa homem...💔");
+        addError("descricaoTarefa", "Passa alguma coisa homem...💔");
     }
+}
+/* -------------------------------------------------------------------------- */
+/*                                    UTILS                                   */
+/* -------------------------------------------------------------------------- */
+function $(id) {
+    return document.getElementById(id);
+}
+function addError(id, message, time) {
+    if (time === void 0) { time = 1500; }
+    var errorElement = document.createElement("div");
+    var errorsArea = document.getElementById(id);
+    errorsArea.appendChild(errorElement);
+    errorElement.classList.add("mensagemErro");
+    errorElement.textContent = message;
+    setTimeout(function () {
+        errorsArea.removeChild(errorElement);
+    }, time);
 }
--- DOM-2/12-tasks/script.ts
+++ DOM-2/12-tasks/script.ts
@@ -1,5 +1,3 @@
-import { $, addError } from "../lib/utils";
-
 document.addEventListener("DOMContentLoaded", main);
 
 type Task = {
@@ -10,29 +8,63 @@ type Task = {
 };
 
 const tasks: Task[] = [];
+let contadorId = 1;
 
 function main() {
+  console.log("entrei na main");
   const button = $("adicionarBtn");
   button?.addEventListener("click", addTask);
 }
 
+function concludeTask(button: HTMLElement) {
+  console.log("lindinho");
+  const trow = button.parentElement?.parentElement;
+  const id = Array.from(trow?.getElementsByClassName("taskId") ?? []).at(0);
+  console.log(id?.textContent);
+  const task = tasks.find((task) => task.id === Number(id?.textContent));
+  console.log(task);
+  if (task) {
+    task.dataConclusao = new Date();
+    updateRows();
+  }
+}
+
 function updateRows() {
   const tbody = $("tabelaTarefas")?.querySelector("tbody");
+  const existingTrows = Array.from(
+    document.getElementsByClassName("taskId") ?? []
+  ).map((id) => Number(id.textContent) ?? NaN);
+  const filteredTasks = tasks.filter(
+    (task) => !existingTrows.find((id) => id === task.id)
+  );
   const trows: HTMLTableRowElement[] = [];
-  for (let task of tasks) {
+  for (let task of filteredTasks) {
     const trow = document.createElement("tr");
     const id = document.createElement("td");
     const descricao = document.createElement("td");
     const dataInicio = document.createElement("td");
     const dataConclusao = document.createElement("td");
+    const actionsButtons = document.createElement("td");
+    const finishButton = document.createElement("button");
+    const excludeButton = document.createElement("button");
+
+    id.className = "taskId";
     id.textContent = `${task.id}`;
     descricao.textContent = `${task.descricao}`;
     dataInicio.textContent = `${task.dataInicio.toLocaleDateString()}`;
-    dataConclusao.textContent = `${task.dataConclusao?.toLocaleDateString()}`;
+    dataConclusao.textContent = `${
+      task.dataConclusao?.toLocaleDateString() ?? "----"
+    }`;
+    finishButton.textContent = "Concluir";
+    finishButton.addEventListener("click", () => concludeTask(finishButton));
+    excludeButton.textContent = "Excluir";
+    actionsButtons.appendChild(finishButton);
+    actionsButtons.appendChild(excludeButton);
     trow.appendChild(id);
     trow.appendChild(descricao);
     trow.appendChild(dataInicio);
     trow.appendChild(dataConclusao);
+    trow.appendChild(actionsButtons);
     trows.push(trow);
   }
   if (trows.length) {
@@ -41,10 +73,11 @@ function updateRows() {
 }
 
 function addTask() {
+  console.log("chamei da");
   const input = $("descricaoTarefa") as HTMLInputElement;
   if (input?.value) {
     const newTask: Task = {
-      id: tasks.length + 1,
+      id: contadorId++,
       descricao: input.value.trim(),
       dataInicio: new Date(),
     };
@@ -53,4 +86,23 @@ function addTask() {
   } else {
     addError("descricaoTarefa", "Passa alguma coisa homem...💔");
   }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                    UTILS                                   */
+/* -------------------------------------------------------------------------- */
+
+function $(id: string) {
+  return document.getElementById(id);
+}
+
+function addError(id: string, message: string, time: number = 1500) {
+  var errorElement = document.createElement("div");
+  var errorsArea = document.getElementById(id);
+  errorsArea!.appendChild(errorElement);
+  errorElement.classList.add("mensagemErro");
+  errorElement.textContent = message;
+  setTimeout(function () {
+    errorsArea!.removeChild(errorElement);
+  }, time);
 }
